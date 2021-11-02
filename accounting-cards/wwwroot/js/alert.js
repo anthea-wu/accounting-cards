910e0f363e44bdde84e094c549973597ae13bfc6
anthea <anthea@emct.com.tw>
2021-11-02 11:10:38 +0800

    更新：登入後提示

--- accounting-cards/wwwroot/js/alert.js
+++ accounting-cards/wwwroot/js/alert.js
@@ -3,7 +3,17 @@ function alert_success(title, msg) {
         icon: 'success',
         title: title,
         text: msg,
-        timer: 1500,
+        timer: 3000,
         confirmButtonColor: '#28a745',
+    })
+}
+
+function alert_fail(title, msg) {
+    Swal.fire({
+        icon: 'error',
+        title: title,
+        text: msg,
+        timer: 3000,
+        confirmButtonColor: '#dc3545',
     })
 }
