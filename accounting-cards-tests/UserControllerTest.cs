9139e720921481a6cdf29ce223d823915fa56689
anthea <anthea@emct.com.tw>
2021-11-08 18:05:14 +0800

    Unit test：當帳號不存在於資料庫中時取得step=0

---
+++ accounting-cards-tests/UserControllerTest.cs
@@ -0,0 +1,52 @@
+﻿using accounting_cards.Controllers;
+using accounting_cards.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace accounting_cards_tests
+{
+    [TestFixture]
+    public class UserControllerTest
+    {
+        static AccountingContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<AccountingContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb")
+                .Options;
+            return new AccountingContext(options);
+        }
+        private static readonly AccountingContext _context = GetDbContext();
+        private static readonly UserTestService _userService = new UserTestService();
+
+        private readonly UserController _userController = new(_context, _userService, new ResultService());
+
+        [Test]
+        public void Return_Step_One_When_Account_Is_New()
+        {
+            // Arrange
+            var account = new UserCheckRequestBindingModel();
+            
+            // Act
+            var okResult = _userController.Check(account) as OkObjectResult;
+            var result = okResult.Value as UserCheckResponseBindingModel;
+
+            // Assert
+            Assert.AreEqual(0, result.Step);
+        }
+        
+    }
+
+    class UserTestService : IDbService
+    {
+        public User? GetExistOrCreateNew(UserCheckRequestBindingModel account, string salt)
+        {
+            return new User()
+            {
+                account = account.Account,
+                temp_key = salt
+            };
+        }
+    } 
+}
