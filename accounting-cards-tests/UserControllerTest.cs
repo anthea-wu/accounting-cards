04af972f8fff9b83e9b9dabf30677fd5c987c103
anthea <anthea@emct.com.tw>
2021-11-08 18:14:54 +0800

    Unit test：當帳號存在於資料庫中時取得step=1

--- accounting-cards-tests/UserControllerTest.cs
+++ accounting-cards-tests/UserControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using accounting_cards.Controllers;
+﻿using System;
+using accounting_cards.Controllers;
 using accounting_cards.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -17,13 +18,13 @@ namespace accounting_cards_tests
                 .Options;
             return new AccountingContext(options);
         }
-        private static readonly AccountingContext _context = GetDbContext();
+        private static readonly AccountingContext _db = GetDbContext();
         private static readonly UserTestService _userService = new UserTestService();
 
-        private readonly UserController _userController = new(_context, _userService, new ResultService());
+        private readonly UserController _userController = new(_db, _userService, new ResultService());
 
         [Test]
-        public void Return_Step_One_When_Account_Is_New()
+        public void Return_Step_Zero_When_Account_Is_New()
         {
             // Arrange
             var account = new UserCheckRequestBindingModel();
@@ -35,6 +36,31 @@ namespace accounting_cards_tests
             // Assert
             Assert.AreEqual(0, result.Step);
         }
+
+        [Test]
+        public void Return_Step_One_When_Account_Is_Exist()
+        {
+            // Arrange
+            var data = new User()
+            {
+                guid = Guid.NewGuid(),
+                account = "unit-test-test",
+            };
+            _db.Users.Add(data);
+            _db.SaveChanges();
+            
+            var account = new UserCheckRequestBindingModel()
+            {
+                Account = "unit-test-test"
+            };
+            
+            // Act
+            var okResult = _userController.Check(account) as OkObjectResult;
+            var result = okResult.Value as UserCheckResponseBindingModel;
+            
+            // Assert
+            Assert.AreEqual(1, result.Step);
+        }
         
     }
 
@@ -42,6 +68,15 @@ namespace accounting_cards_tests
     {
         public User? GetExistOrCreateNew(UserCheckRequestBindingModel account, string salt)
         {
+            if (string.Equals(account.Account, "unit-test-test"))
+            {
+                return new User()
+                {
+                    account = account.Account,
+                    password = "123",
+                    temp_key = salt
+                };
+            }
             return new User()
             {
                 account = account.Account,
